8877f0a9f5b79dbd2a570e4cf1f94875e42739f2
ADRIANO F PEREIRA <estudoadriano@gmail.com>
2025-08-16 20:40:45 -0300

    Update ex4_function.js

--- Aula 06 - Function_JS/ex4_function.js
+++ Aula 06 - Function_JS/ex4_function.js
@@ -9,24 +9,12 @@ const produtos = [
     'Manteiga',
     'Chocolate'
 ];
-
-// for (let i = 0; i < produtos.length; i++) {
-//     if (produtos[i].startsWith('A'))
-//         produtosA.push(produtos[i]);
-// }
-// console.log(produtosA)
-
 const produtosA= () => {
-    for (const item of produtos) 
+    for (const item of produtos) {
         if (item.toLocaleLowerCase().includes('a')) {
             console.log(`${item} contem a letra a`)
         }
+    }
 }
 produtosA()
 
-// const filtrar_e = () => {
-//     for (const item of produtos) 
-//         if (item.toLocaleLowerCase().includes('e')) {
-//             console.log(`${item} contem a letra e`)
-//         }
-// }
